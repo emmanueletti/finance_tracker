3a4a2c9ee9ebf679980b769107050571a4ba46fc
Emmanuel Etti <emmanueletti@gmail.com>
2022-04-23 13:21:40 -0700

    switches response to ajazx

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,13 +1,25 @@
 class StocksController < ApplicationController
   def search
     if params[:stock].empty?
-      flash[:alert] = 'Please enter a symbol to search'
-      redirect_to my_portfolio_path
+      flash.now[:alert] = 'Please enter a symbol to search'
+      respond_to do |format|
+        format.js { render partial: 'users/result' }
+      end
     else
       # using the Stock model
       # also creating a instance variable so the view can have access to it
       @stock = Stock.new_lookup(params[:stock])
-      render 'users/my_portfolio'
+      if @stock
+        # AJAX reponse - makes use of _result.js.erb
+        respond_to do |format|
+          format.js { render partial: 'users/result' }
+        end
+      else
+        flash.now[:alert] = 'Please enter a valid symbol to search'
+        respond_to do |format|
+          format.js { render partial: 'users/result' }
+        end
+      end
     end
   end
 end
--- app/models/stock.rb
+++ app/models/stock.rb
@@ -4,24 +4,6 @@ class Stock < ApplicationRecord
   # able to use the method
 
   def self.new_lookup(ticker_symbol)
-    return if ticker_symbol.empty?
-    return unless ticker_symbol.is_a? String
-
-    Stock.initialize_client
-
-    begin
-      # will return an instantiation of the Stock class
-      # don't need to write Stock.new since we are already inside the Stock class
-      # Check the schema for all the properties needed to instantiate a model Class
-      company = client.company(ticker_symbol)
-    rescue StandardError
-      nil
-    else
-      new(ticker: ticker_symbol, name: company.company_name, last_price: client.price(ticker_symbol))
-    end
-  end
-
-  def make_api_call
     # command: EDITOR="code --wait" rails credentials:edit to open up decrypted credentials
     # file with vscode as the text editor
     client = IEX::Api::Client.new(
@@ -29,8 +11,14 @@ class Stock < ApplicationRecord
       endpoint: 'https://sandbox.iexapis.com/v1'
     )
 
-    { "name": client.company(ticker_symbol) }
+    begin
+      # will return an instantiation of the Stock class
+      # don't need to write Stock.new since we are already inside the Stock class
+      # Check the schema for all the properties needed to instantiate a model Class
+      new(ticker: ticker_symbol, name: client.company(ticker_symbol).company_name,
+          last_price: client.price(ticker_symbol))
+    rescue StandardError => e
+      nil
+    end
   end
-
-  def self.initialize_client; end
 end
