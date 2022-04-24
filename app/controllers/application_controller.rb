b8c7391482b654adb9e446af0834675d692c1736
Emmanuel Etti <emmanueletti@gmail.com>
2022-04-23 20:20:17 -0700

    adds ability to update user account with first and last name

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 class ApplicationController < ActionController::Base
   # From the devise docs
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[first_name last_name])
+  end
 end
